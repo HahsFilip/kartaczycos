3f515e6da9759ec2572e72a76813f03d9a1abcd3
HahsFilip <33375677+HahsFilip@users.noreply.github.com>
2019-02-20 20:50:07 +0100

    dodane obrazki do testow feature detection

--- mian.py
+++ mian.py
@@ -1,19 +1,39 @@
-import cv2
 import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
-filename = 'obrazki/testkarta.png'
-img = cv2.imread(filename)
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img1 = cv2.imread('box.png',0)          # queryImage
+img2 = cv2.imread('box_in_scene.png',0) # trainImage
 
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray,2,3,0.04)
+# Initiate SIFT detector
+sift = sift = cv2.xfeatures2d.SIFT_create()
 
-#result is dilated for marking the corners, not important
-dst = cv2.dilate(dst, None)
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
 
-# Threshold for an optimal value, it may vary depending on the image.
-img[dst>0.01*dst.max()]=[0,0,255]
+# FLANN parameters
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks=50)   # or pass empty dictionary
 
-cv2.imshow('dst', img)
-if cv2.waitKey(0) & 0xff == 27:
-    cv2.destroyAllWindows()
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+# Need to draw only good matches, so create a mask
+matchesMask = [[0,0] for i in xrange(len(matches))]
+
+# ratio test as per Lowe's paper
+for i,(m,n) in enumerate(matches):
+    if m.distance < 0.7*n.distance:
+        matchesMask[i]=[1,0]
+
+draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = 0)
+
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+
+plt.imshow(img3,),plt.show()
